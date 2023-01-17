dd8e6dc6299511c72dec4c94417054e8a3ce085e
ranimELHAJ <60525147+Ranim5@users.noreply.github.com>
2023-01-17 19:04:51 +0100

    letssgoo

--- src/app/shared/Service/Facture.service.ts
+++ src/app/shared/Service/Facture.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FactureService {
-  readonly API_URL = 'http://192.168.1.141:8089/SpringMVC/facture';
+  readonly API_URL = 'http://192.168.49.1:8089/SpringMVC/facture';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://192.168.1.141:8089/SpringMVC/operateur';
+  readonly API_URL = 'http://192.168.49.1:8089/SpringMVC/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
